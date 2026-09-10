e1c1410c9f4f24b39b8a08ef539ab6dcd97c21c2
TTulenchick <dzars.ttulenchick@gmail.com>
2026-09-10 05:35:07 +0000

    Courses

---
+++ DzyubanLabReact/src/App.jsx
@@ -0,0 +1,28 @@
+import Header from "./Components/Header"
+import Hero from "./Components/Hero"
+import Courses from "./Components/Courses"
+import Testimonials from "./Components/Testimonials"
+import Footer from "./Components/Footer"
+function App() {
+
+  return (
+    <>
+      {/*  ==================== HEADER ====================  */}
+  <Header/>
+
+  {/*  ==================== HERO ====================  */}
+  <Hero/>
+
+  {/*  ==================== COURSES ====================  */}
+  <Courses/>
+
+  {/*  ==================== TESTIMONIALS ====================  */}
+  <Testimonials/>
+
+  {/*  ==================== FOOTER ====================  */}
+  <Footer/>
+      </>
+  )
+}
+
+export default App
